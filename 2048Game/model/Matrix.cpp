b3faed4fcd7b7ced6894a97c38174bc05627fede
Brownlzy <73223120+Brownlzy@users.noreply.github.com>
2023-06-05 14:26:37 +0800

    修改Matrix

--- 2048Game/model/Matrix.cpp
+++ 2048Game/model/Matrix.cpp
@@ -1,1 +1,80 @@
 #include "Matrix.h"
+#include <QDebug>
+
+Matrix::Matrix()
+{
+	for(int i=0;i<4;i++){
+		for(int j=0;j<4;j++){
+			matrix[i][j] = 0;
+		}
+	}
+}
+
+Matrix::Matrix(const Matrix& ma)
+{
+	for(int i=0;i<4;i++){
+		for(int j=0;j<4;j++){
+			matrix[i][j] = ma.matrix[i][j];
+		}
+	}
+}
+
+Matrix::~Matrix()
+{
+}
+
+int Matrix::getNumberIn(int x, int y)
+{
+	return matrix[x][y];
+}
+
+void Matrix::setNumberIn(int x, int y, const int value)
+{
+	matrix[x][y] = value;
+}
+
+int* Matrix::getLineOnX(int x)
+{
+	return matrix[x];
+}
+
+void Matrix::setLineOnX(int x, const int* values)
+{
+	for (int i = 0; i < 4; i++) {
+		matrix[x][i]=values[i];
+	}
+}
+
+int* Matrix::getLineOnY(int y)
+{
+	int linex[4];
+	for (int i = 0; i < 4; i++) {
+		linex[i] = matrix[i][y];
+	}
+	return linex;
+}
+
+void Matrix::setLineOnY(int y, const int* values)
+{
+	for (int i = 0; i < 4; i++) {
+		matrix[i][y]=values[i];
+	}
+}
+
+void Matrix::printToConsole()
+{
+	QString line;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			line.append(QString::number(matrix[i][j])+"\t\t");
+		}
+		qDebug().noquote() << line;
+		line = "";
+	}
+}
+
+void Matrix::printToConsole(QString title)
+{
+	qDebug().noquote() << title;
+	printToConsole();
+}
--- 2048Game/model/Matrix.h
+++ 2048Game/model/Matrix.h
@@ -1,14 +1,24 @@
 #pragma once
+#include <QString>
 class Matrix
 {
 private:
     int matrix[4][4];
 public:
+    Matrix();
+    Matrix(const Matrix& ma);
+    ~Matrix();
     /*获得位于(x,y)处的方块值(0为空)*/
     int getNumberIn(int x, int y);
+    void setNumberIn(int x, int y,const int value);
     /*获得位于x行的方块值(0为空)*/
     int* getLineOnX(int x);
+    void setLineOnX(int x,const int* values);
     /*获得位于y行的方块值(0为空)*/
     int* getLineOnY(int y);
+    void setLineOnY(int y,const int* values);
+
+    void printToConsole();
+    void printToConsole(QString title);
 };
 
