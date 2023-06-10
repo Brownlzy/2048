d7fbef0cd77b02a7432268b5395ba8e3bf4b4867
ShakingX <73223305+ShakingX@users.noreply.github.com>
2023-06-10 15:07:00 +0800

    将每局产生分数存入map中

--- 2048Game/controller/MainControl.h
+++ 2048Game/controller/MainControl.h
@@ -2,18 +2,20 @@
 #include "../abstract/GameUI.h"
 #include "../abstract/ControllListener.h"
 #include "../generate/generate.h"
+#include <map>
 class MainControl:public ControllListener
 {
 private:
 	GameUI* gui;
 	Matrix* matrix;
+	static int round;
+	std::map<int, int>records;
 	generate gen;
 	int score=0;
 	void init();
 
 public:
 	MainControl(GameUI* gui);
-
 	void judgeEnd(Matrix matrix);
 	void onArrowControl(Direction control);
 	void onFuncControl(FuncControl control);
